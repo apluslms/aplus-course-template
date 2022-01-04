298361be9624c856f5928430db2938cfa78fd357
Markku Riekkinen <markku.riekkinen@aalto.fi>
2022-01-04 20:35:27 +0200

    Remove git commands from build.sh
    With the new Git Manager service, these should be removed from build.sh.

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-git submodule init
-git submodule update
-
 # touchrst ensures that all changes in the RST or config YAML files
 # are always taken into account in the compilation (make html).
 make touchrst html
